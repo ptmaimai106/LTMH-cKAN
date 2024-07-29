0f8d316b04233103be0ecb83a46c4cc054c9857e
ptmaimai106 <ptmaimai106@gmail.com>
2024-07-29 10:09:16 +0700

    experiment on fashion-mnist

---
+++ train_fashion_mnist.py
@@ -0,0 +1,302 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import FashionMNIST
+
+from torch.utils.data import DataLoader
+
+from architectures_28x28.CKAN_BN import CKAN_BN
+from architectures_28x28.SimpleModels import *
+from architectures_28x28.ConvNet import ConvNet
+from architectures_28x28.KANConvs_MLP import KANC_MLP
+from architectures_28x28.KKAN import KKAN_Convolutional_Network
+from architectures_28x28.conv_and_kan import NormalConvsKAN
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+# Cargar MNIST y filtrar por dos clases
+mnist_train = FashionMNIST(root='./data', train=True, download=True, transform=transform)
+mnist_test = FashionMNIST(root='./data', train=False, download=True, transform=transform)
+
+# DataLoader
+train_loader = DataLoader(mnist_train, batch_size=64, shuffle=True)
+test_loader = DataLoader(mnist_test, batch_size=64, shuffle=False)
+
+
+def train(model, device, train_loader, optimizer, epoch, criterion):
+    """
+    Train the model for one epoch
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        train_loader: DataLoader for training data
+        optimizer: the optimizer to use (e.g. SGD)
+        epoch: the current epoch
+        criterion: the loss function (e.g. CrossEntropy)
+
+    Returns:
+        avg_loss: the average loss over the training set
+    """
+
+    model.to(device)
+    model.train()
+    train_loss = 0
+    # Process the images in batches
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        # Recall that GPU is optimized for the operations we are dealing with
+        data, target = data.to(device), target.to(device)
+        
+        # Reset the optimizer
+        optimizer.zero_grad()
+        
+        # Push the data forward through the model layers
+        output = model(data)
+        
+        # Get the loss
+        loss = criterion(output, target)
+
+        # Keep a running total
+        train_loss += loss.item()
+        
+        # Backpropagate
+        loss.backward()
+        optimizer.step()
+        
+    # return average loss for the epoch
+    avg_loss = train_loss / (batch_idx+1)
+    # print('Training set: Average loss: {:.6f}'.format(avg_loss))
+    return avg_loss
+
+def test(model, device, test_loader, criterion):
+    """
+    Test the model
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        test_loader: DataLoader for test data
+        criterion: the loss function (e.g. CrossEntropy)
+
+    Returns:
+        test_loss: the average loss over the test set
+        accuracy: the accuracy of the model on the test set
+        precision: the precision of the model on the test set
+        recall: the recall of the model on the test set
+        f1: the f1 score of the model on the test set
+    """
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    all_targets = []
+    all_predictions = []
+    
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            
+            # Get the predicted classes for this batch
+            output = model(data)
+            
+            # Calculate the loss for this batch
+            test_loss += criterion(output, target).item()
+            
+            # Calculate the accuracy for this batch
+            _, predicted = torch.max(output.data, 1)
+            correct += (target == predicted).sum().item()
+
+            # Collect all targets and predictions for metric calculations
+            all_targets.extend(target.view_as(predicted).cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
+    # Calculate overall metrics
+    precision = precision_score(all_targets, all_predictions, average='macro')
+    recall = recall_score(all_targets, all_predictions, average='macro')
+    f1 = f1_score(all_targets, all_predictions, average='macro')
+
+    # Normalize test loss
+    test_loss /= len(test_loader.dataset)
+    accuracy = correct / len(test_loader.dataset)
+
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%), Precision: {:.2f}, Recall: {:.2f}, F1 Score: {:.2f}\n'.format(
+    #     test_loss, correct, len(test_loader.dataset), accuracy, precision, recall, f1))
+
+    return test_loss, accuracy, precision, recall, f1
+
+def train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs, scheduler, save_dir):
+    """
+    Train and test the model
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        train_loader: DataLoader for training data
+        test_loader: DataLoader for test data
+        optimizer: the optimizer to use (e.g. SGD)
+        criterion: the loss function (e.g. CrossEntropy)
+        epochs: the number of epochs to train
+        scheduler: the learning rate scheduler
+
+    Returns:
+        all_train_loss: a list of the average training loss for each epoch
+        all_test_loss: a list of the average test loss for each epoch
+        all_test_accuracy: a list of the accuracy for each epoch
+        all_test_precision: a list of the precision for each epoch
+        all_test_recall: a list of the recall for each epoch
+        all_test_f1: a list of the f1 score for each epoch
+    """
+    # Track metrics
+    all_train_loss = []
+    all_test_loss = []
+    all_test_accuracy = []
+    all_test_precision = []
+    all_test_recall = []
+    all_test_f1 = []
+
+    train_and_test_models.best_test_loss = float('inf')
+    
+    for epoch in range(1, epochs + 1):
+        # Train the model
+        train_loss = train(model, device, train_loader, optimizer, epoch, criterion)
+        all_train_loss.append(train_loss)
+        
+        # Test the model
+        test_loss, test_accuracy, test_precision, test_recall, test_f1 = test(model, device, test_loader, criterion)
+        all_test_loss.append(test_loss)
+        all_test_accuracy.append(test_accuracy)
+        all_test_precision.append(test_precision)
+        all_test_recall.append(test_recall)
+        all_test_f1.append(test_f1)
+
+        print(f'End of Epoch {epoch}: Train Loss: {train_loss:.6f}, Test Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.2%}')
+        scheduler.step()
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        
+        # Save the model if it has the best validation loss
+        if test_loss < train_and_test_models.best_test_loss:
+            train_and_test_models.best_test_loss = test_loss
+            model_save_path = os.path.join(save_dir, 'best_model.pth')
+            torch.save(model.state_dict(), model_save_path)
+
+    model.all_test_accuracy = all_test_accuracy
+    model.all_test_precision = all_test_precision
+    model.all_test_f1 = all_test_f1
+    model.all_test_recall = all_test_recall
+
+    return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1, train_and_test_models.best_test_loss
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+print("CUDA is available:", torch.cuda.is_available())
+print("Number of GPUs:", torch.cuda.device_count())
+print("Current CUDA device:", torch.cuda.current_device())
+print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+
+# Training for SimpleCNN
+model_SimpleCNN = SimpleCNN()
+model_SimpleCNN.to(device)
+optimizer_SimpleCNN = optim.AdamW(model_SimpleCNN.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_SimpleCNN = optim.lr_scheduler.ExponentialLR(optimizer_SimpleCNN, gamma=0.8)
+criterion_SimpleCNN = nn.CrossEntropyLoss()
+simpleCNN_dir = "models/fashion-mnist/SimpleCNN"
+all_train_loss_SimpleCNN, all_test_loss_SimpleCNN, all_test_accuracy_SimpleCNN, all_test_precision_SimpleCNN, all_test_recall_SimpleCNN, all_test_f1_SimpleCNN, best_test_loss = train_and_test_models(
+    model_SimpleCNN, 
+    device, 
+    train_loader, 
+    test_loader, 
+    optimizer_SimpleCNN, 
+    criterion_SimpleCNN, 
+    epochs=10, 
+    scheduler=scheduler_SimpleCNN, 
+    save_dir=simpleCNN_dir)
+
+
+
+# Training for SimpleCNN_2
+model_SimpleCNN_2 = SimpleCNN_2()
+model_SimpleCNN_2.to(device)
+optimizer_SimpleCNN_2 = optim.AdamW(model_SimpleCNN_2.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_SimpleCNN_2 = optim.lr_scheduler.ExponentialLR(optimizer_SimpleCNN_2, gamma=0.8)
+criterion_SimpleCNN_2 = nn.CrossEntropyLoss()
+simpleCNN_2_dir = "models/fashion-mnist/SimpleCNN_2"
+all_train_loss_SimpleCNN_2, all_test_loss_SimpleCNN_2, all_test_accuracy_SimpleCNN_2, all_test_precision_SimpleCNN_2, all_test_recall_SimpleCNN_2, all_test_f1_SimpleCNN_2, best_test_loss_SimpleCNN_2 = train_and_test_models(
+    model_SimpleCNN_2, 
+    device, 
+    train_loader, 
+    test_loader, 
+    optimizer_SimpleCNN_2, 
+    criterion_SimpleCNN_2, 
+    epochs=10, 
+    scheduler=scheduler_SimpleCNN_2,
+      save_dir=simpleCNN_2_dir)
+
+
+model_SimpleLinear = SimpleLinear()
+model_SimpleLinear.to(device)
+optimizer_SimpleLinear = optim.AdamW(model_SimpleLinear.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_SimpleLinear = optim.lr_scheduler.ExponentialLR(optimizer_SimpleLinear, gamma=0.8)
+criterion_SimpleLinear = nn.CrossEntropyLoss()
+simpleLinear_dir = "models/fashion-mnist/SimpleLinear"
+all_train_loss_SimpleLinear, all_test_loss_SimpleLinear, all_test_accuracy_SimpleLinear, all_test_precision_SimpleLinear, all_test_recall_SimpleLinear, all_test_f1_SimpleLinear, best_test_loss_SimpleLinear = train_and_test_models(
+    model_SimpleLinear, 
+    device, 
+    train_loader, 
+    test_loader,
+    optimizer_SimpleLinear, 
+    criterion_SimpleLinear, 
+    epochs=10, 
+    scheduler=scheduler_SimpleLinear,
+    save_dir=simpleLinear_dir
+)
+
+
+model_ConvNet = ConvNet()
+model_ConvNet.to(device)
+optimizer_ConvNet = optim.AdamW(model_ConvNet.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_ConvNet = optim.lr_scheduler.ExponentialLR(optimizer_ConvNet, gamma=0.8)
+criterion_ConvNet = nn.CrossEntropyLoss()
+convNet_dir = "models/fashion-mnist/ConvNet"
+all_train_loss_ConvNet, all_test_loss_ConvNet, all_test_accuracy_ConvNet, all_test_precision_ConvNet, all_test_recall_ConvNet, all_test_f1_ConvNet, best_test_loss_ConvNet = train_and_test_models(model_ConvNet, device, train_loader, test_loader, optimizer_ConvNet, criterion_ConvNet, epochs=10, scheduler=scheduler_ConvNet, save_dir=convNet_dir)
+
+
+
+model_KKAN_Convolutional_Network = KKAN_Convolutional_Network(device = device)
+model_KKAN_Convolutional_Network.to(device)
+optimizer_KKAN_Convolutional_Network = optim.AdamW(model_KKAN_Convolutional_Network.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_KKAN_Convolutional_Network = optim.lr_scheduler.ExponentialLR(optimizer_KKAN_Convolutional_Network, gamma=0.8)
+criterion_KKAN_Convolutional_Network = nn.CrossEntropyLoss()
+kkan_dir="models/fashion-mnist/KKAN_Convolutional"
+all_train_loss_KKAN_Convolutional_Network, all_test_loss_KKAN_Convolutional_Network, all_test_accuracy_KKAN_Convolutional_Network, all_test_precision_KKAN_Convolutional_Network, all_test_recall_KKAN_Convolutional_Network, all_test_f1_KKAN_Convolutional_Network, best_test_loss_KKAN = train_and_test_models(model_KKAN_Convolutional_Network, device, train_loader, test_loader, optimizer_KKAN_Convolutional_Network, criterion_KKAN_Convolutional_Network, epochs=10, scheduler=scheduler_KKAN_Convolutional_Network, save_dir=kkan_dir)
+
+
+
+model_KANC_MLP= KANC_MLP(device = device)
+model_KANC_MLP.to(device)
+optimizer_KANC_MLP = optim.AdamW(model_KANC_MLP.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_KANC_MLP = optim.lr_scheduler.ExponentialLR(optimizer_KANC_MLP, gamma=0.8)
+criterion_KANC_MLP = nn.CrossEntropyLoss()
+kanc_mlp_dir="models/fashion-mnist/KANC_MLP"
+all_train_loss_KANC_MLP, all_test_loss_KANC_MLP, all_test_accuracy_KANC_MLP, all_test_precision_KANC_MLP, all_test_recall_KANC_MLP, all_test_f1_KANC_MLP, best_test_loss_KANC_MLP = train_and_test_models(model_KANC_MLP, device, train_loader, test_loader, optimizer_KANC_MLP, criterion_KANC_MLP, epochs=10, scheduler=scheduler_KANC_MLP, save_dir=kanc_mlp_dir)
+
+
+
+model_Convs_and_KAN= NormalConvsKAN()
+model_Convs_and_KAN.to(device)
+optimizer_Convs_and_KAN = optim.AdamW(model_Convs_and_KAN.parameters(), lr=1e-3, weight_decay=1e-4)
+scheduler_Convs_and_KAN = optim.lr_scheduler.ExponentialLR(optimizer_Convs_and_KAN, gamma=0.8)
+criterion_Convs_and_KAN = nn.CrossEntropyLoss()
+normal_convs_kan_dir="models/fashion-mnist/NormalConvsKAN"
+all_train_loss_Convs_and_KAN, all_test_loss_Convs_and_KAN, all_test_accuracy_Convs_and_KAN, all_test_precision_Convs_and_KAN, all_test_recall_Convs_and_KAN, all_test_f1_Convs_and_KAN, best_test_loss_NormalConvsKan = train_and_test_models(model_Convs_and_KAN, device, train_loader, test_loader, optimizer_Convs_and_KAN, criterion_Convs_and_KAN, epochs=10, scheduler=scheduler_Convs_and_KAN, save_dir=normal_convs_kan_dir)
+
+
